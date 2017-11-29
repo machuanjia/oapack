57d188e5619305aecbd20e882a2c511065154cb0
machuanjia <machuanjia@worktile.com>
2017-11-29 18:01:42 +0800

    add iview

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
+import { Button, Table } from 'iview'
+import 'iview/dist/styles/iview.css'
 
 Vue.use(BootstrapVue)
+Vue.component('Button', Button)
+Vue.component('Table', Table)
 
 /* eslint-disable no-new */
 new Vue({
